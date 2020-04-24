57b40359edb528f66bd7c66030db1e560fbb3285
burn0ut7 <46172991+burn0ut7@users.noreply.github.com>
2020-04-23 21:03:29 -0400

    Update script_component.hpp

--- functions/script_component.hpp
+++ functions/script_component.hpp
@@ -1,2 +1,4 @@
 #include "\z\ace\addons\medical_damage\script_component.hpp"
 #include "\z\ace\addons\medical_engine\script_component.hpp"
+#include "\z\ace\addons\medical_engine\script_macros_medical.hpp"
+#include "\z\ace\addons\main\script_macros.hpp"
